c1aed163f17230ea881f601b05f9d27804c07619
isofinly <isofinly@protonmail.com>
2022-12-03 00:15:53 +0300

    another poor code commit

--- place/TypeOfPlaces.java
+++ place/TypeOfPlaces.java
@@ -2,18 +2,20 @@ package place;
 
 public enum TypeOfPlaces {
     HILL("Hill"),
+    HOME("Home"),
+    ROCKET_PLACE("Rocket place"),
     PLAINS("Plains"),
     BUNCHES("Bunches"),
     VERANDA("Verand"),
     SQUARE("Square"),
     INDISTANCE("Indistance");
-    private final String name;
+    private final String placesName;
 
-    TypeOfPlaces(String name) {
-        this.name = name;
+    TypeOfPlaces(String placesName) {
+        this.placesName = placesName;
     }
 
-    public String getName() {
-        return name;
+    public String getPlacesName() {
+        return placesName;
     }
 }
