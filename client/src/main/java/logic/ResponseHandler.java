842019726adf6ba14ea0f952bbf749145916404c
isofinly <isofinly@icloud.com>
2023-04-04 12:12:03 +0300

    chunking?

--- client/src/main/java/logic/ResponseHandler.java
+++ client/src/main/java/logic/ResponseHandler.java
@@ -5,15 +5,13 @@ import interfaces.CLI;
 import interfaces.UI;
 import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.Logger;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.StreamCorruptedException;
+
+import java.io.*;
 import java.net.PortUnreachableException;
 import java.nio.ByteBuffer;
 import java.util.Scanner;
 
-public class ResponseHandler extends Thread{
+public class ResponseHandler extends Thread {
 
     RequestHandler requestHandler;
     UI ui;
@@ -26,29 +24,95 @@ public class ResponseHandler extends Thread{
 
     @Override
     public void run() {
-        while(requestHandler.isRunnable()) {
+        while (requestHandler.isRunnable()) {
             if (requestHandler.getChannel().isOpen()) {
                 try {
-                    byte[] incomingData = new byte[8196*8196];
-                    ByteBuffer byteBuffer = ByteBuffer.wrap(incomingData);
-                    requestHandler.getChannel().receive(byteBuffer);
-                    byteBuffer.flip();
-                    ByteArrayInputStream in = new ByteArrayInputStream(incomingData);
-                    ObjectInputStream is = new ObjectInputStream(in);
-                    OutputData answer = (OutputData) is.readObject();
-                    logger.info("OutputData has been received.");
-                    logger.warn(answer.getResultMessage());
-                    if (answer.getResultMessage().equals("connected")) {
-                        requestHandler.setConnected(true);
-                        System.out.println("Connection with server has been established!");
-                        logger.info("Connection with server has been established!");
-                    } else {
-                        ui.display(answer);
+
+//                    ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+//                    byte[] buffer = new byte[8196];
+//                    int bytesRead;
+//                    int totalBytesRead = 0;
+//                    int expectedDataLength = -1;
+//                    while ((bytesRead = requestHandler.getChannel().read(ByteBuffer.wrap(buffer))) > 0) {
+//                        byteStream.write(buffer, 0, bytesRead);
+//                        totalBytesRead += bytesRead;
+//                        if (expectedDataLength == -1 && totalBytesRead >= 4) {
+//                            // First 4 bytes indicate the expected length of the incoming data
+//                            expectedDataLength = ByteBuffer.wrap(byteStream.toByteArray(), 0, 4).getInt();
+//                        }
+//                        if (totalBytesRead >= expectedDataLength) {
+//                            // We've received all the data, break out of the loop
+//                            break;
+//                        }
+//                    }
+//                    byte[] incomingData = byteStream.toByteArray();
+//                    if (incomingData.length > 0) {
+//                        ByteArrayInputStream in = new ByteArrayInputStream(incomingData);
+//                        ObjectInputStream is = new ObjectInputStream(in);
+//                        OutputData answer = (OutputData) is.readObject();
+//                        logger.info("OutputData has been received.");
+//                        logger.warn(answer.getResultMessage());
+//                        if (answer.getResultMessage().equals("connected")) {
+//                            requestHandler.setConnected(true);
+//                            System.out.println("Connection with server has been established!");
+//                            logger.info("Connection with server has been established!");
+//                        } else {
+//                            ui.display(answer);
+//                        }
+//                    }
+
+//                    ByteBuffer buffer = ByteBuffer.allocate(8196*8196);
+//                    ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+//                    int bytesRead;
+//                    while ((bytesRead = requestHandler.getChannel().read(buffer)) > 0) {
+//                        buffer.flip();
+//                        byteStream.write(buffer.array(), 0, bytesRead);
+//                        buffer.clear();
+//                    }
+//                    byte[] incomingData = byteStream.toByteArray();
+//                    if (incomingData.length > 0) {
+//                        ByteArrayInputStream in = new ByteArrayInputStream(incomingData);
+//                        ObjectInputStream is = new ObjectInputStream(in);
+//                        OutputData answer = (OutputData) is.readObject();
+//                        logger.info("OutputData has been received.");
+//                        logger.warn(answer.getResultMessage());
+//                        if (answer.getResultMessage().equals("connected")) {
+//                            requestHandler.setConnected(true);
+//                            System.out.println("Connection with server has been established!");
+//                            logger.info("Connection with server has been established!");
+//                        } else {
+//                            ui.display(answer);
+//                        }
+//                    }
+
+
+                    ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[8196*8916];
+                    int bytesRead;
+                    int totalBytesRead = 0;
+                    while ((bytesRead = requestHandler.getChannel().read(ByteBuffer.wrap(buffer))) > 0) {
+                        byteStream.write(buffer, 0, bytesRead);
+                        totalBytesRead += bytesRead;
+                    }
+                    byte[] incomingData = byteStream.toByteArray();
+                    if (incomingData.length > 0) {
+                        ByteArrayInputStream in = new ByteArrayInputStream(incomingData);
+                        ObjectInputStream is = new ObjectInputStream(in);
+                        OutputData answer = (OutputData) is.readObject();
+                        logger.info("OutputData has been received.");
+                        logger.warn(answer.getResultMessage());
+                        if (answer.getResultMessage().equals("connected")) {
+                            requestHandler.setConnected(true);
+                            System.out.println("Connection with server has been established!");
+                            logger.info("Connection with server has been established!");
+                        } else {
+                            ui.display(answer);
+                        }
                     }
                 } catch (PortUnreachableException e) {
-                    ui.display("Error","Не удалось подключиться к серверу");
+                    ui.display("Error", "Не удалось подключиться к серверу");
                 } catch (ClassNotFoundException e) {
-                    ui.display("Error","Не удалось определить класс, возможно что-то с сериализацией данных или недостаток размера буфера");
+                    ui.display("Error", "Не удалось определить класс, возможно что-то с сериализацией данных или недостаток размера буфера");
                     requestHandler.setRunnable(false);
                 } catch (StreamCorruptedException e) {
 //                    ui.display("Error","Какие-то проблемы с потоком данных");
@@ -59,3 +123,4 @@ public class ResponseHandler extends Thread{
         }
     }
 }
+
